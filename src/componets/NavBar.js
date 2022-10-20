2971b9b4c07378eddbabc07bdc7f1d1307ba3099
ajithkr12 <ajithkr.akr012kaippayil@gmail.com>
2022-10-20 16:26:21 +0530

    validate

--- src/componets/NavBar.js
+++ src/componets/NavBar.js
@@ -15,35 +15,37 @@ const NavBar = () =>  {
 	};
 
 	return (
-		<nav className="navbar">
-			<Link to="/" className="nav-logo">
-                <img src={Logo} alt="Logo"/>
-			</Link>
-			<div onClick={handleClick} className="nav-icon">
-				{open ? <FiX /> : <FiMenu />}
-			</div>
-			<ul className={open ? 'nav-links active' : 'nav-links'}>
-				<li className="nav-item">
-					<Link to="/" className="nav-link" onClick={closeMenu}>
-						Home
-					</Link>
-				</li>
-				<li className="nav-item">
-					<Link to="/about" className="nav-link" onClick={closeMenu}>
-						About
-					</Link>
-				</li>
-				<li className="nav-item">
-					<Link to="/blogs" className="nav-link" onClick={closeMenu}>
-						Blogs
-					</Link>
-				</li>
-				<li className="nav-item">
-					<Link to="/contact" className="nav-link" onClick={closeMenu}>
-						Contact
-					</Link>
-				</li>
-			</ul>
-		</nav>
+		<div className="navarea">
+			<nav className="navbar">
+				<Link to="/" className="nav-logo">
+					<img src={Logo} alt="Logo"/>
+				</Link>
+				<div onClick={handleClick} className="nav-icon">
+					{open ? <FiX /> : <FiMenu />}
+				</div>
+				<ul className={open ? 'nav-links active' : 'nav-links'}>
+					<li className="nav-item">
+						<Link to="/" className="nav-link" onClick={closeMenu}>
+							Home
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/about" className="nav-link" onClick={closeMenu}>
+							About
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/blogs" className="nav-link" onClick={closeMenu}>
+							Blogs
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/contact" className="nav-link" onClick={closeMenu}>
+							Contact
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</div>
   	);}
 export default NavBar;
--- src/screens/ContactUs.js
+++ src/screens/ContactUs.js
@@ -1,7 +1,9 @@
 
 import React from 'react';
-
+import {useForm }from "react-hook-form";
 const ContactUs = () =>  {
+    // const { register, handleSubmit, errors } = useForm();
+    const {register,handleSubmit,formState: { errors },} = useForm();
 
   return (
  
@@ -35,32 +37,41 @@ const ContactUs = () =>  {
                 <div className="col-lg-6 col-12">
                     <div className="contact-form">
                         <h3 className="form-title">Leave a message here</h3>
-                        <form className="form" method="post" action="">
+                        <form className="form" onSubmit={handleSubmit((data) => console.log(data))}>
                             <div className="row">
                                 <div className="col-lg-6 col-md-6 col-12">
                                     <div className="form-group">
-                                        <input name="name" type="text" placeholder="Name*" required="required"/>
+                                        <input type="text" placeholder="Name" {...register('Name', { required: true,minLength: 2})} />
+                                        {errors.Name && errors.Name.type === "required" && <p className="red">Name is required.</p>}
+                                        {errors.Name && errors.Name.type === "minLength" && (<p className="red">Your name must be at least 2 characters.</p>)}
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-6 col-md-6 col-12">
+                                    <div className="form-group">
+                                        <input type="text" placeholder="Email" {...register('email', { required: true,pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i})} />
+                                        {errors.email && errors.email.type === "required" && <p className="red">Email is required.</p>}
+                                        {errors.email && errors.email.type === "pattern" && (<p className="red">Invalid Email Address</p>)}
                                     </div>
                                 </div>
                                 <div className="col-lg-6 col-md-6 col-12">
                                     <div className="form-group">
-                                        <input name="email" type="email" placeholder="Email*" required="required"/>
+                                        <input type="text" placeholder="Subject" {...register('subject', { required: true,maxLength:50})} />
+                                        {errors.subject && errors.subject.type === "required" && <p className="red">Subject is required.</p>}
+                                        {errors.subject && errors.subject.type === "maxLength" && (<p className="red">more content</p>)}
                                     </div>
                                 </div>
                                 <div className="col-lg-6 col-md-6 col-12">
                                     <div className="form-group">
-                                        <input name="subject" type="text" placeholder="Subject*" required="required"/>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6 col-md-6 col-12">
-                                    <div className="form-group">
-                                        <input name="phone" type="text" placeholder="Phone*" required="required"/>
+                                        <input type="tel" placeholder="Mobile Number" {...register('Mobile', { required: true,minLength: 6, maxLength: 12})} />
+                                        {errors.Mobile && errors.Mobile.type === "required" && <p className="red">Mobile is required.</p>}
+                                        {errors.Mobile && errors.Mobile.type === "minLength" && errors.Mobile.type === "maxLength" &&(<p className="red">Invalid Mobile Number</p>)}
                                     </div>
                                 </div>
                                 <div className="col-12">
                                     <div className="form-group">
-                                        <textarea placeholder="Message*" name="message" id="message-area"
-                                        className="form-control"></textarea>
+                                        <textarea className="form-control" placeholder="Message" id="message-area"{...register('message', { required: true})}/>
+                                        {errors.message && errors.message.type === "required" && <p className="red">Message is required.</p>}
                                     </div>
                                 </div>
                                 <div className="col-12">
